aeb3847fedee3b2538ea8043fa6d9f9e3d7a6a0e
vimkidgit <1779156598@qq.com>
2017-09-21 16:52:35 +0800

    updarte

--- src/Rand.php
+++ src/Rand.php
@@ -11,28 +11,28 @@
 
 namespace Kidphp\KidphpRand;
 class Rand {
-	/** 生成指定数量不重复随机数
-	 * @begin  随机数开始值
-	 * @end    随机数结束值
-	 * @times  生成数量
-	 */
-	function noRepeatRand($begin,$end,$times){
-		//生成数量不能大于随机数数量
-		if($times>$end-$begin+1){
-			return array(
-				'error'=>1,
-				'errorInfo'=>'生成数量不能大于随机数数量'
-			);
-		}
-		$tmp=array();
-		while(count($tmp)<$times){
-			$tmp[]=mt_rand($begin,$end);
-			$tmp=array_unique($tmp);
-		}
-		return array(
-			'error'=>0,
-			'errorInfo'=>'',
-			'data'=>$tmp
-		);
-	}
+    /** 生成指定数量不重复随机数
+     * @begin  随机数开始值
+     * @end    随机数结束值
+     * @times  生成数量
+     */
+    function noRepeatRand($begin,$end,$times){
+        //生成数量不能大于随机数数量
+        if($times>$end-$begin+1){
+            return array(
+                'error'=>1,
+                'errorInfo'=>'生成数量不能大于随机数数量'
+            );
+        }
+        $tmp=array();
+        while(count($tmp)<$times){
+            $tmp[]=mt_rand($begin,$end);
+            $tmp=array_unique($tmp);
+        }
+        return array(
+            'error'=>0,
+            'errorInfo'=>'',
+            'data'=>$tmp
+        );
+    }
 }
